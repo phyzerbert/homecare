3b5dad22eca83fe7b883314224b518a98f3a9081
phyzerbert <luckydev2288@gmail.com>
2020-03-24 05:44:56 +1000

    seed

--- resources/lang/en/page.php
+++ resources/lang/en/page.php
@@ -6,7 +6,7 @@ return [
     'health_topics' => 'Health Topics',
     'pre_order' => 'Pre-Order',
     'coronavirus' => 'Coronavirus',
-    'disposable_sanitary_protective_mask' => 'Disposable Sanitary Protective Mask',
+    'disposable_sanitary_protective_mask' => 'Disposable sanitary protective 3ply face mask',
     'price' => 'Price Per Box',
     'quantity' => 'Number of Products',
     'buy_now' => 'Buy',
